3af0264cbaec6d39e0da6b2ccb327e042311d536
Andrew Bell <constant4337@molecularmail.com>
2025-02-13 11:47:13 -0700

    Splice dataset

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
 def main():
     file = open("dataset.txt", 'r')
-    vocab = set(file.read())
-    print(vocab)
+    text = file.read()
+    spliceIndex = int(len(text) * 0.9)
+
+    first = text[:spliceIndex]
+    last = text[spliceIndex:]
+
+    vocab = set(text)
 
 
 if __name__ == "__main__":
