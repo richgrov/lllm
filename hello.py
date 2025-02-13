5cf312e781e2be22491f11b0f9e70755bb011ec6
Andrew Bell <constant4337@molecularmail.com>
2025-02-13 12:41:09 -0700

    jax setup

--- hello.py
+++ hello.py
@@ -1,4 +1,16 @@
 import random as rand
+import jax.numpy as jnp
+from jax import grad, jit, vmap
+
+def predict(params, inputs):
+    for W, b in params:
+        outputs = jnp.dot(inputs, W) + b
+        inputs = jnp.tanh(outputs)  # inputs to the next layer
+    return outputs                # no activation on last layer
+
+def loss(params, inputs, targets):
+    preds = predict(params, inputs)
+    return jnp.sum((preds - targets)**2)
 
 def main():
     file = open("dataset.txt", 'r')
@@ -14,8 +26,10 @@ def main():
     
     for character in vocab:
         random_vec = []
+
         for vec_idx in range(63):
             random_vec.append(rand.uniform(-1.0, 1.0))
+
         randomized_vocab_vectors[character] = random_vec
     
     # for idx in range(len(shakespear)):
@@ -23,6 +37,11 @@ def main():
     print(randomized_vocab_vectors)
     print(vocab)
 
+    grad_loss = jit(grad(loss))  # compiled gradient evaluation function
+    perex_grads = jit(vmap(grad_loss, in_axes=(None, 0, 0)))  # fast per-example grads
+
+    print(grad_loss)
+    print(perex_grads)
 
 if __name__ == "__main__":
     main()
